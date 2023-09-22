f3ba0a575615d3e66ac6da91b118fd5ede5cb1c6
NGallop <n.p.gallop@gmail.com>
2023-09-22 14:59:01 +0100

    added skeleton structure to repo

---
+++ dops/challenges.py
@@ -0,0 +1,6 @@
+###############################################################################
+# Author: Niall Gallop                                                        #
+# Date of Creation: 22/09/2023                                                #
+# Last Updated:                                                               #
+# Description: challenges.py - house answers to DOPS exercises set by DE      #
+###############################################################################
