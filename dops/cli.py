dd335bf79f656dfcd8ff732b9171850352b4afd6
NGallop <n.p.gallop@gmail.com>
2023-09-25 10:30:08 +0100

    modified cli to suit 3 argument format (protein translate, reverse complement, md5sum)

--- dops/cli.py
+++ dops/cli.py
@@ -15,40 +15,33 @@ class cli_obj():
         parser = argparse.ArgumentParser(description='')
 
         parser.add_argument(
-            '-i', '--input', required = True, help='Input file')
-        parser.add_argument(
-            '-f', '--genbank_format', action='store_true',
-            help='Reformat the sequence in gebank format')
+            '-i', '--input', required = True, help='Input file'
+        )
+
         parser.add_argument(
             '-p', '--protein', action='store_true',
-            help='Translate DNA sequence to amino acid sequence')
+            help='Translate DNA sequence to amino acid sequence'
+        )
         parser.add_argument(
             '-c', '--reverse_complement', action='store_true',
-            help='Convert sequence to the reverse complement')
+            help='Convert sequence to the reverse complement'
+        )
+
         parser.add_argument(
-            '-r', '--reading_frames', action='store_true',
-            help='Return all 6 reading frames for a given sequence')
-        parser.add_argument(
-            '-b', '--base_counting', action='store_true',
-            help='Count all mono-, di-, and tri-nucleotide instances in a seq.')
-        parser.add_argument(
-            '-g', '--gc_content', action='store_true',
-            help='Return the GC-content of a given sequence as a percentage')
+            '-md', '--md5sum', action='store_true',
+            help='perform an md5sum check of a file'
+        )
         
         self.args = parser.parse_args(sys_args)
 
     def arg_selection(self):
-        selected = [False, False, False, False, False, False]
-        if (self.args.genbank_format == True):
-            selected[0] = True
+        selected = [False, False, False]
+
         if (self.args.protein == True):
-            selected[1] = True
+            selected[0] = True
         if (self.args.reverse_complement == True):
+            selected[1] = True
+        if (self.args.md5sum == True):
             selected[2] = True
-        if (self.args.reading_frames == True):
-            selected[3] = True
-        if (self.args.base_counting == True):
-            selected[4] = True
-        if (self.args.gc_content == True):
-            selected[5] = True
+
         return selected
